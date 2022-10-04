55ef32bf4934c98e6fc013ab23f918594acbcd6b
marchen03 <marian.chen@student.maastrichtuniversity.nl>
2022-10-04 21:28:19 +0200

    It works!

--- base.py
+++ base.py
@@ -8,11 +8,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.permanent_session_lifetime = timedelta(minutes=5)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:hazelnoot2022@localhost/flask'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'hazelnoot2022'
-app.config['MYSQL_DB'] = 'flask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:hazelnoot2022@localhost/flask'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'hazelnoot2022'
+# app.config['MYSQL_DB'] = 'flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
